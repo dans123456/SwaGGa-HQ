6d56a436678a48b15f395b07dd9e9cff5a8d12a8
jmens12 <johanmens11@gmail.com>
2026-05-31 16:22:28 +0000

    chore: Bump Service Worker cache to v49 to force cache invalidation and apply fixes

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Strategy: Cache-first for static assets, network-first for dynamic.
  */
 
-const CACHE_NAME = 'swagga-hq-v48';
+const CACHE_NAME = 'swagga-hq-v49';
 
 // Core files to cache on install
 const CORE_ASSETS = [
